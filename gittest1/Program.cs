0ae1fd1dc02235cd02ba9fcd0f03bac5835e20d0
tanweerahmad9 <140061768+tanweerahmad9@users.noreply.github.com>
2023-07-24 15:32:03 +0530

    Update Program.cs
    updates the timestamp or put some wait on it

--- gittest1/Program.cs
+++ gittest1/Program.cs
@@ -13,7 +13,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection("use time stamp");
 app.UseStaticFiles();
 
 app.UseRouting("HELLO Tanweer");
